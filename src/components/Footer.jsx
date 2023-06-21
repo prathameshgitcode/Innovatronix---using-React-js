eec2248e044d9539c420f58ef3b58a9c8602505d
prathameshgitcode <prathameshdalvi340@gmail.com>
2023-06-21 13:37:15 +0530

    Add untracked files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Innovatronix</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a
+            href="https://www.youtube.com/channel/UCQb1LRWVCYvljWwjJoz70HQ"
+            target={"blank"}
+          >
+            Youtube
+          </a>
+          <a
+            href="https://www.instagram.com/prathameshdalvi240/"
+            target={"blank"}
+          >
+            Instagram
+          </a>
+          <a href="https://github.com/prathameshgitcode" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Home.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import vg from "../assets/2.webp";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home" id="home">
+        <main>
+          <h1>Innovatronix</h1>
+          <p>Revolutionizing Solutions for Success</p>
+        </main>
+      </div>
+
+      <div className="home2">
+        <img src={vg} alt="Graphics" />
+
+        <div>
+          <p>
+            We are your one and only solution to the tech problems you face
+            every day. We are leading tech company whose aim is to increase the
+            problem solving ability in children
+          </p>
+        </div>
+      </div>
+
+      <div className="home3" id="about">
+        <div>
+          <h1>Who we are?</h1>
+          <p>
+            Welcome to Innovatronix, At Innovatronix, we are a trusted provider
+            of comprehensive software solutions. With a team of skilled
+            professionals, we design and develop customized software
+            applications to address your unique business challenges. Our
+            expertise spans across various industries, delivering scalable and
+            secure solutions that drive efficiency and growth. From concept to
+            deployment, we prioritize collaboration and client satisfaction.
+            Partner with us to transform your ideas into innovative software
+            solutions that propel your business forward.
+          </p>
+        </div>
+      </div>
+
+      <div className="home4" id="brands">
+        <div>
+          <h1>Brands</h1>
+          <article>
+            <div
+              style={{
+                animationDelay: "0.3s",
+              }}
+            >
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+            <div
+              style={{
+                animationDelay: "0.5s",
+              }}
+            >
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+            <div
+              style={{
+                animationDelay: "0.7s",
+              }}
+            >
+              <AiFillYoutube />
+              <p>Youtube</p>
+            </div>
+            <div
+              style={{
+                animationDelay: "1s",
+              }}
+            >
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
